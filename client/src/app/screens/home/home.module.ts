20875ff3590f0a85d9e1131bafbe4e2e8f56263e
EryomenkoAlexander <sasha.eryomenko77@gmail.com>
2023-01-07 22:56:54 +0600

    complete greeting title

--- client/src/app/screens/home/home.module.ts
+++ client/src/app/screens/home/home.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from "@angular/core";
 import {HomeComponent} from "./home.component";
+import {GreetingComponent} from "./greeting/greeting.component";
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    GreetingComponent
   ],
   imports: [
 
