5b86ec227815cdce524077eb64698d894b7c2918
EryomenkoAlexander <sasha.eryomenko77@gmail.com>
2023-01-08 18:56:22 +0600

    create button

--- client/src/app/screens/home/home.module.ts
+++ client/src/app/screens/home/home.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from "@angular/core";
 import {HomeComponent} from "./home.component";
 import {GreetingComponent} from "./greeting/greeting.component";
+import {FormModule} from "../../shared/form/form.module";
 
 @NgModule({
   declarations: [
@@ -8,7 +9,7 @@ import {GreetingComponent} from "./greeting/greeting.component";
     GreetingComponent
   ],
   imports: [
-
+    FormModule
   ],
   exports: [
     HomeComponent
